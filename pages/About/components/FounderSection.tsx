12846708b64d38c16c871bb237e5235e5f901ce9
Suhan| Fullstack Developer <greensoulit811@gmail.com>
2026-05-14 13:57:00 +0600

    update for button

--- pages/About/components/FounderSection.tsx
+++ pages/About/components/FounderSection.tsx
@@ -7,7 +7,7 @@ const FounderSection: React.FC = () => {
     <section className="py-24 bg-white">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="relative flex flex-col lg:flex-row bg-white rounded-[1.5rem] overflow-hidden shadow-[0_20px_50px_rgba(0,0,0,0.08)] border border-gray-100">
-          
+        
           {/* Image Side */}
           <div className="lg:w-1/2 relative h-[400px] lg:h-auto overflow-hidden">
             <img 
